84d23a195be22ec1c8df29a7d94200ba5486a694
AntonKralin <antonkralin91@gmail.com>
2023-09-30 16:52:04 +0300

    task2 done

--- tasks/views.py
+++ tasks/views.py
@@ -1,11 +1,13 @@
-from django.http import HttpRequest, JsonResponse
+from django.http import HttpRequest, JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 
 import json
+import os
 
 from .functions import parse_json, save_robot
 from .exceptions import ValidJSON, DublicateData
+from .report import create_file
 
 
 # Create your views here.
@@ -25,3 +27,15 @@ def add(request: HttpRequest):
         return JsonResponse({'success': False, 'error': str(ex)})
     except ValueError:
         return JsonResponse({'success': False, 'error': 'ValueError'})
+
+
+@require_http_methods(["GET"])
+def report(request: HttpRequest):
+    filename = create_file(7)
+    if os.path.exists(filename):
+        with open(filename, mode='rb') as fh:
+            response = HttpResponse(fh.read(),
+                                    content_type="application/ms-excel")
+            response['Content-Disposition'] = 'inline; filename=' + \
+                os.path.basename(filename)
+        return response
